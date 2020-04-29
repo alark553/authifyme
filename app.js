e58e9878d990c59f44933ee3d907b8cb584dc85d
Aman Agrawal <aman.agrawal@ad.loginradius.net>
2020-04-29 12:13:15 +0530

    Added node passport strateagy for twitter

---
+++ app.js
@@ -0,0 +1,38 @@
+const passport = require('passport')
+const TwitterStrategy = require('passport-twitter').Strategy;
+
+const TWITTER_CONSUMER_KEY = "mamdfhjhhfjh";
+const TWITTER_CONSUMER_SECRET = "jdshfgjhsdghg";
+
+
+var express = require('express');
+var app = express();
+
+passport.use(new TwitterStrategy({
+    consumerKey: TWITTER_CONSUMER_KEY,
+    consumerSecret: TWITTER_CONSUMER_SECRET,
+    callbackURL: "http://www.example.com/auth/twitter/callback"
+  },
+  function(token, tokenSecret, profile, done) {
+    console.log("token, tokenSecret, profile ", token, tokenSecret, profile);
+    // User.findOrCreate(..., function(err, user) {
+    //   if (err) { return done(err); }
+    //   done(null, user);
+    // });
+  }
+));
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+  res.render('pages/index');
+});
+
+app.listen(8080);
+console.log('8080 is the magic port');
+
+app.get('/auth/twitter', passport.authenticate('twitter'));
+
+app.get('/auth/twitter/callback',
+  passport.authenticate('twitter', { successRedirect: '/',
+                                     failureRedirect: '/login' }));
