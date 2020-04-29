15be431b0ae61c18396ab5d3f794bb59c8d6a66f
Aman Agrawal <aman.agrawal@ad.loginradius.net>
2020-04-29 12:47:31 +0530

    Added PORt variable

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.get('/', function(req, res) {
   res.render('pages/index');
 });
 
-app.listen(8080);
+app.listen(process.argv[2]);
 console.log('8080 is the magic port');
 
 app.get('/auth/twitter', passport.authenticate('twitter'));
